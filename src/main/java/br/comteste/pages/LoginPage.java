265173e354d575ace3a369a3c61f0c86465e1897
AlexJuniorTIQa <alexjunior5851@yahoo.com.br>
2020-10-23 01:25:11 -0300

    Mapeamento dos elementos da tela de Login

--- src/main/java/br/comteste/pages/LoginPage.java
+++ src/main/java/br/comteste/pages/LoginPage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class CadastroPage {
+public class LoginPage {
 
-	@FindBy(how = How.XPATH, using = "//input[@class='SearchBar__input ']")
-	public WebElement barraDePesquisa;
+	@FindBy(how = How.ID, using = "email")
+	public WebElement campoEmail;
 	
-	@FindBy(how = How.XPATH, using = "//div[@class='SearchBar__results__result__name']")
-	public WebElement resultadoDaPesquisa;
-
+	@FindBy(how = How.ID, using = "passwd")
+	public WebElement campoPassword;
+	
+	@FindBy(how = How.ID, using = "SubmitLogin")
+	public WebElement btnSignIn;
+	
+	
 }
