3a19386e66cbff325ac48cb38eb61e5045c352ec
AlexJuniorTIQa <alexjunior5851@yahoo.com.br>
2020-10-23 01:25:23 -0300

    Step de execução dos testes

--- src/test/java/br/com/teste/steps/LoginComSucessoStep.java
+++ src/test/java/br/com/teste/steps/LoginComSucessoStep.java
@@ -5,38 +5,59 @@ import org.openqa.selenium.WebDriver;
 import br.com.teste.driver.DriverFactory;
 import br.com.teste.driver.Screenshot;
 import br.com.teste.excel.MassaDeDados;
-import br.com.teste.logic.CadastroPageLogic;
+import br.com.teste.logic.AccountLogic;
 import br.com.teste.logic.GenericLogic;
 import br.com.teste.logic.HomeLogic;
-import cucumber.api.PendingException;
+import br.com.teste.logic.LoginLogic;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 
-public class LoginStep {
+public class LoginComSucessoStep {
 
 	private WebDriver driver;
 	private String folder;
 	private HomeLogic homePageLogic;
-	private CadastroPageLogic estoqueLogic;
+	private LoginLogic loginLogic;
+	private AccountLogic accountLogic;
 	private MassaDeDados massaDeDados;
 
 	@Before
 	public void Driver() throws Exception {
 		driver = DriverFactory.getDriver();
 		homePageLogic = new HomeLogic(driver);
-		estoqueLogic = new CadastroPageLogic(driver);
+		accountLogic = new AccountLogic(driver);
+		loginLogic = new LoginLogic(driver);
 		massaDeDados = new MassaDeDados();
-		folder = "AutomationPractice" + GenericLogic.getTimeStamp();
+		folder = "Teste de Login com sucesso " + GenericLogic.getTimeStamp();
 
 	}
 
-	@Dado("que eu esteja na HomePage do site Automation Practice")
-	public void que_eu_esteja_na_HomePage_do_site_Automation_Practice(){
-	   
+	@Dado("que eu esteja na HomePage do site AutomationPractice")
+	public void que_eu_esteja_na_HomePage_do_site_AutomationPractice() throws Exception {
+		Screenshot.getScreenShot(driver, folder, "Home Page");
+	}
+
+	@Quando("clico em 'Sign In")
+	public void clico_em_Sign_In() throws Throwable {
+		homePageLogic.clicaSignIn();
+		Screenshot.getScreenShot(driver, folder, "Tela de Login");
+	}
+
+	@Quando("preencho o campos de login")
+	public void preencho_o_campos_de_login() throws Throwable {
+		loginLogic.insereEmail(massaDeDados.getEmailUserValido());
+		loginLogic.inserePassword(massaDeDados.getPasswordUserValido());
+		Screenshot.getScreenShot(driver, folder, "Dados De Login Preenchidos");
+	}
+
+	@Entao("ao clicar em 'Sign In' efetuo o login com sucesso")
+	public void ao_clicar_em_Sig_In_efetuo_o_meu_login() throws Throwable {
+		loginLogic.clicaSignIn();
+		accountLogic.validaTituloMyAccount();
+		Screenshot.getScreenShot(driver, folder, "Tela De Login efetuado com Sucesso");
 	}
 
 	@After
